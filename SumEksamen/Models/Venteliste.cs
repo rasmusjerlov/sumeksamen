d165914462aa976d9e2078d141e3d08dd8e6a33f
AbukarMoallin <abukarabdi184@gmail.com>
2024-11-07 12:50:57 +0100

    abu changes

--- SumEksamen/Models/Venteliste.cs
+++ SumEksamen/Models/Venteliste.cs
@@ -27,7 +27,7 @@ public class Venteliste
         }
         else
         {
-            throw new ArgumentException("Elev skal være under 18 år");
+            throw new ArgumentException("Elev skal være under 18 år.");
         }
     }
 
