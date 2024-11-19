9d8ead8ff06f79fca040d54c60c161bf12179486
Mazza <olivergiuliano00@gmail.com>
2024-11-19 09:28:30 +0100

    Færdiggjort Værelser

--- SumEksamen.Tests/Controllers/VærelseControllerTest.cs
+++ SumEksamen.Tests/Controllers/VærelseControllerTest.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using JetBrains.Annotations;
 using SumEksamen.Controllers;
 using SumEksamen.Models;
@@ -12,12 +13,16 @@ public class VærelseControllerTest
     
     public VærelseControllerTest()
     {
-        værelseController = new VærelseController();
-        værelseController.OpretVærelse("1", 4);
+        værelseController = new VærelseController(new VentelisteController());
+        værelseController.OpretVærelse(3);
+        List<Elev> elevListe = new List<Elev>();
+        
         Elev e1 = new Elev("mikkel", Køn.dreng);
         Elev e2 = new Elev("julian", Køn.dreng);
         Elev e3 = new Elev("mads", Køn.dreng);
         Elev e4 = new Elev("Martin", Køn.dreng);
+        
+        
         
     }
     
@@ -25,6 +30,7 @@ public class VærelseControllerTest
     [Fact]
     public void TC1_FordelEleverPåVærelser()
     {
+       
         
         
         
